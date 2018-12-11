70bc8ea3928a9ea34a2d62c48aef47b777c0303c
Mustafa Ünal <39712938+RevengeFire@users.noreply.github.com>
2018-12-11 13:02:08 +0300

    Create soru3.c

---
+++ soru3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int func(int a[10]) {
+    int kontrol=0;
+    for(int j=100;j<999;j++){
+        for(int i=2;i<j;i++){
+        if(j%i==0){break;}
+        else {a[kontrol]=j;kontrol++;break;}
+    }
+    }return 0;
+}
+int main(){
+    int arr[10];
+    func(arr[10]);
+    for(int i=0;i<10;i++)printf("dizinin %d. elemanı : %d\n",i+1,arr[i]);
+}
